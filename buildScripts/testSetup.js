57f279e72e9c4f25a6f37ca87bcc89089c6ed2d9
Taibu Haroub <haroubtaibu@gmail.com>
2021-07-26 02:48:54 +0300

    Configure automated testing

---
+++ buildScripts/testSetup.js
@@ -0,0 +1,7 @@
+// This file isn't transpiled, so must use CommonJS and ES5
+
+// Register babel to transpile before our test run.
+require("@babel/register")();
+
+// Disable wepack features that Mocha doesn't understand.
+require.extensions[".css"] = function() {};
