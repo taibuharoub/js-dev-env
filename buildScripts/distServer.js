b328460cefbcbf31aa8c79c5fbe5cfe9147cc792
Taibu Haroub <haroubtaibu@gmail.com>
2021-07-27 03:39:06 +0300

    Setup server to run built production verion

---
+++ buildScripts/distServer.js
@@ -0,0 +1,36 @@
+//Run the production built version
+import path from "path";
+
+import open from "open";
+import express from "express";
+import compression from "compression";
+
+
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
+
+const app = express();
+const port = 3000;
+
+app.use(compression());
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.get("/", (req, res, next) => {
+  res.sendFile(path.join(__dirname, "../dist/index.html"));
+});
+
+//Mock an api call
+app.get("/users", (req, res, next) => {
+  res.json([
+    { id: 1, firName: "Ty", lastNmae: "haro", email: "ty@ty100.com" },
+    { id: 2, firName: "Taibu", lastNmae: "haroub", email: "taibu@ty100.com" },
+  ]);
+});
+
+app.listen(port, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    open("http://localhost:" + port);
+  }
+});
