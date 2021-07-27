3e340d47bb1b2de54f576ff6fead4aa5c903dd7f
Taibu Haroub <haroubtaibu@gmail.com>
2021-07-28 02:43:53 +0300

    Minify CSS for production

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,16 +1,25 @@
 import path from "path";
 import HtmlWebpackPlugin from "html-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
 export default {
   mode: "production",
   devtool: "source-map",
-  entry: "./src/index.js",
+  entry: {
+    main: path.resolve(__dirname, "src/index"),
+    vendor: path.resolve(__dirname, "src/vendor")
+  },
   output: {
     path: path.resolve(__dirname, "dist"),
     publicPath: "/",
-    filename: "bundle.js",
+    filename: "[name].[chunkhash].js",
   },
   plugins: [
+    //Generate an external css file with a hash in filename
+    new MiniCssExtractPlugin({
+      filename: "[name].[chunkhash].css"
+    }),
+    //Create Html file that include reference to bundled JS
     new HtmlWebpackPlugin({
       template: "src/index.html"
     })
@@ -18,7 +27,7 @@ export default {
   module: {
     rules: [
       { test: /\.js$/, exclude: /node_modules/, use: ["babel-loader"] },
-      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+      { test: /\.css$/, use: [MiniCssExtractPlugin.loader, "css-loader"] },
     ],
   },
 };
